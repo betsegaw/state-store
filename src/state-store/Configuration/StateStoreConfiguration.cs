3be4002cb0196a33b5a35c33a51db7fc7914e68d
Betsegaw Tadele <betsegaw.ta@gmail.com>
2018-06-12 07:49:30 -0700

    Create the events api controller with a sample api to access the currently active name

--- src/state-store/Configuration/StateStoreConfiguration.cs
+++ src/state-store/Configuration/StateStoreConfiguration.cs
@@ -1,37 +1,45 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
-public class StateStoreConfiguration
+namespace state_store.Configuration
 {
-    private static StateStoreConfiguration stateStoreConfigurationInstance;
+    public class StateStoreConfiguration
+    {
+        private static StateStoreConfiguration stateStoreConfigurationInstance;
 
-    public static StateStoreConfiguration StateStoreConfigurationInstance {
-        get {
-            if (stateStoreConfigurationInstance == null) {
-                YamlDotNet.Serialization.Deserializer x = new YamlDotNet.Serialization.Deserializer();
-                stateStoreConfigurationInstance = x.Deserialize<StateStoreConfiguration>(File.ReadAllText(@"C:\Users\betse\Source\Repos\state-store\src\state-store\SampleConfigurations\SampleConfiguration.yaml"));
+        public static StateStoreConfiguration StateStoreConfigurationInstance
+        {
+            get
+            {
+                if (stateStoreConfigurationInstance == null)
+                {
+                    YamlDotNet.Serialization.Deserializer x = new YamlDotNet.Serialization.Deserializer();
+                    stateStoreConfigurationInstance = x.Deserialize<StateStoreConfiguration>(File.ReadAllText($"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}/SampleConfiguration.yaml"));
+                }
+
+                return stateStoreConfigurationInstance;
             }
-
-            return stateStoreConfigurationInstance;
         }
+        public string Name { get; set; }
+        public string Token { get; set; }
+        public List<ConditionConfigurations> ConditionConfigurations { get; set; }
     }
-    public string Name { get; set; }
-    public string Token { get; set; }
-    public List<ConditionConfigurations> ConditionConfigurations { get; set; }
-}
 
-public class Condition
-{
-    public string Name { get; set; }
-    public bool Value { get; set; }
-    public string ActionURL { get; set; }
-}
+    public class Condition
+    {
+        public string Name { get; set; }
+        public bool Value { get; set; }
+        public string ActionURL { get; set; }
+    }
 
-public class ConditionConfigurations
-{
-    public string Name { get; set; }
-    public string URL { get; set; }
-    public string ConditionSourceURL { get; set; }
-    public Condition ConditionTrueValue { get; set; }
-    public Condition ConditionFalseValue { get; set; }
+    public class ConditionConfigurations
+    {
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public string ConditionSourceURL { get; set; }
+        public Condition ConditionTrueValue { get; set; }
+        public Condition ConditionFalseValue { get; set; }
+    }
+
 }
